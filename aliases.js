26e7893a11a45eea134070eb25ca9b065d63f481
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-15 20:16:21 +0000

    Deploy pebble-os/pebble to github.com/pebble-os/pebble.git:gh-pages

--- aliases.js
+++ aliases.js
@@ -5,4 +5,4 @@ ALIASES['cfg_if'] = {};
 ALIASES['kernel'] = {};
 ALIASES['log'] = {};
 ALIASES['spin'] = {};
-ALIASES['x86_64'] = {"*":[{'crate':'x86_64','ty':8,'name':'Deref','desc':'Used for immutable dereferencing operations, like `*v`.','p':'x86_64::memory::paging'},{'crate':'x86_64','ty':8,'name':'DerefMut','desc':'Used for mutable dereferencing operations, like in `*v = 1;`.','p':'x86_64::memory::paging'}],"&*":[{'crate':'x86_64','ty':8,'name':'Deref','desc':'Used for immutable dereferencing operations, like `*v`.','p':'x86_64::memory::paging'}],};
+ALIASES['x86_64'] = {"*":[{'crate':'x86_64','ty':8,'name':'Deref','desc':'Used for immutable dereferencing operations, like `*v`.','p':'x86_64::memory::paging'},{'crate':'x86_64','ty':8,'name':'DerefMut','desc':'Used for mutable dereferencing operations, like in `*v =…','p':'x86_64::memory::paging'}],"&*":[{'crate':'x86_64','ty':8,'name':'Deref','desc':'Used for immutable dereferencing operations, like `*v`.','p':'x86_64::memory::paging'}],};
