9c0bf46a6b96afe7cbadfd1bbcb4702d3265450b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-19 22:43:15 +0000

    Deploy pebble-os/pebble to github.com/pebble-os/pebble.git:gh-pages

--- source-files.js
+++ source-files.js
@@ -13,3 +13,4 @@ sourcesIndex["num_rational"] = {"name":"","dirs":[],"files":["lib.rs"]};
 sourcesIndex["num_traits"] = {"name":"","dirs":[{"name":"ops","dirs":[],"files":["checked.rs","inv.rs","mod.rs","mul_add.rs","saturating.rs","wrapping.rs"]}],"files":["bounds.rs","cast.rs","float.rs","identities.rs","int.rs","lib.rs","macros.rs","pow.rs","sign.rs"]};
 sourcesIndex["spin"] = {"name":"","dirs":[],"files":["lib.rs","mutex.rs","once.rs","rw_lock.rs"]};
 sourcesIndex["x86_64"] = {"name":"","dirs":[{"name":"hw","dirs":[],"files":["cpu.rs","gdt.rs","i8259_pic.rs","idt.rs","local_apic.rs","mod.rs","port.rs","registers.rs","serial.rs","tlb.rs","tss.rs"]},{"name":"memory","dirs":[{"name":"paging","dirs":[],"files":["entry.rs","frame.rs","frame_allocator.rs","mapper.rs","mod.rs","page.rs","table.rs"]}],"files":["kernel_map.rs","mod.rs","physical_address.rs","virtual_address.rs"]}],"files":["boot.rs","lib.rs"]};
+createSourceSidebar();
