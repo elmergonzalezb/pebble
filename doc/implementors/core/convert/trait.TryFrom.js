b61594f09788610d6593f03770d84f8eefbebf64
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-01-03 18:54:54 +0000

    Deploy IsaacWoods/pebble to github.com/IsaacWoods/pebble.git:gh-pages

---
+++ doc/implementors/core/convert/trait.TryFrom.js
@@ -0,0 +1,9 @@
+(function() {var implementors = {};
+implementors["libpebble"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.TryFrom.html\" title=\"trait core::convert::TryFrom\">TryFrom</a>&lt;u32&gt; for <a class=\"enum\" href=\"libpebble/syscall/system_object/enum.RequestSystemObjectError.html\" title=\"enum libpebble::syscall::system_object::RequestSystemObjectError\">RequestSystemObjectError</a>","synthetic":false,"types":["libpebble::syscall::system_object::RequestSystemObjectError"]}];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        })()
